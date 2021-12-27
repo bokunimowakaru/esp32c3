bbb7d69bff89d7ac76f4b689693f9f4f4bb4bbca
Wataru KUNINO <git@bokunimo.net>
2021-12-27 20:44:16 +0900

    コメントのインデント

--- learning/ex06_pir/ex06_pir.ino
+++ learning/ex06_pir/ex06_pir.ino
@@ -10,10 +10,10 @@ Practice esp32 14 pir 【Wi-Fi 人感センサ子機】ディープスリープ�
 
                                            Copyright (c) 2016-2019 Wataru KUNINO
 *******************************************************************************/
-#include <WiFi.h>                           // ESP32用WiFiライブラリ
-#include <WiFiUdp.h>                        // UDP通信を行うライブラリ
+#include <WiFi.h>                               // ESP32用WiFiライブラリ
+#include <WiFiUdp.h>                            // UDP通信を行うライブラリ
 #include <HTTPClient.h>                         // HTTPクライアント用ライブラリ
-#include "esp_sleep.h"                      // ESP32用Deep Sleep ライブラリ
+#include "esp_sleep.h"                          // ESP32用Deep Sleep ライブラリ
 
 /******************************************************************************
  LINE Notify 設定
@@ -41,11 +41,11 @@ Practice esp32 14 pir 【Wi-Fi 人感センサ子機】ディープスリープ�
 #define PIN_PIR 1                               // IO 1にセンサ(人感/ドア)を接続
 #define PIN_LED_RGB 2                           // IO2 に WS2812を接続(m5stamp)
 // #define PIN_LED_RGB 8                        // IO8 に WS2812を接続(DevKitM)
-#define SSID "1234ABCD"                     // 無線LANアクセスポイントのSSID
-#define PASS "password"                     // パスワード
-#define PORT 1024                           // 受信ポート番号
-#define DEVICE "pir_s_1,"                   // 人感センサ時デバイス名
-// #define DEVICE "rd_sw_1,"                // ドアセンサ時デバイス名
+#define SSID "1234ABCD"                         // 無線LANアクセスポイントのSSID
+#define PASS "password"                         // パスワード
+#define PORT 1024                               // 受信ポート番号
+#define DEVICE "pir_s_1,"                       // 人感センサ時デバイス名
+// #define DEVICE "rd_sw_1,"                    // ドアセンサ時デバイス名
 
 RTC_DATA_ATTR boolean PIR;                      // pir値のバックアップ保存用
 boolean pir;                                    // 人感センサ値 or ドアセンサ状態値
@@ -53,21 +53,21 @@ esp_deepsleep_gpio_wake_up_mode_t pir_wake;     // 起動用のpir値
 IPAddress IP_BROAD;                             // ブロードキャストIPアドレス
 int wake = (int)esp_sleep_get_wakeup_cause();   // 起動理由を変数wakeに保存
 
-void setup(){                               // 起動時に一度だけ実行する関数
-    pinMode(PIN_PIR,INPUT);                 // センサを接続したポートを入力に
-    pir = digitalRead(PIN_PIR);              // 人感センサの状態を取得
+void setup(){                                   // 起動時に一度だけ実行する関数
+    pinMode(PIN_PIR,INPUT);                     // センサを接続したポートを入力に
+    pir = digitalRead(PIN_PIR);                 // 人感センサの状態を取得
     led_setup(PIN_LED_RGB);                     // WS2812の初期設定(ポート設定)
 
-    Serial.begin(115200);                   // 動作確認のためのシリアル出力開始
+    Serial.begin(115200);                       // 動作確認のためのシリアル出力開始
     Serial.println("ESP32C3 PIR/Reed");         // 「ESP32C3 PIR/Reed」を出力
     Serial.print(" Wake = ");                   // 「wake =」をシリアル出力表示
     Serial.println(wake);                       // 起動理由noをシリアル出力表示
     if(wake == 0) PIR = pir;                    // ボタン以外で起動時にPIR値を設定
     if(wake != 7) sleep();                      // ボタン以外で起動時にスリープ
 
-    WiFi.mode(WIFI_STA);                    // 無線LANをSTAモードに設定
-    WiFi.begin(SSID,PASS);                  // 無線LANアクセスポイントへ接続
-    while(WiFi.status() != WL_CONNECTED){   // 接続に成功するまで待つ
+    WiFi.mode(WIFI_STA);                        // 無線LANをSTAモードに設定
+    WiFi.begin(SSID,PASS);                      // 無線LANアクセスポイントへ接続
+    while(WiFi.status() != WL_CONNECTED){       // 接続に成功するまで待つ
         led((millis()/50) % 10);                // (WS2812)LEDの点滅
         if(millis() > 30000) sleep();           // 30秒超過でスリープ
         delay(50);                              // 待ち時間処理
@@ -78,17 +78,17 @@ void setup(){                               // 起動時に一度だけ実行す
     Serial.println(IP_BROAD);                   // ブロードキャストアドレス表示
 }
 
-void loop(){                                // 繰り返し実行する関数
-    WiFiUDP udp;                            // UDP通信用のインスタンスを定義
+void loop(){                                    // 繰り返し実行する関数
+    WiFiUDP udp;                                // UDP通信用のインスタンスを定義
     udp.beginPacket(IP_BROAD, PORT);            // UDP送信先を設定
-    udp.print(DEVICE);                      // デバイス名を送信
-    udp.print(PIR);                         // 起動時のセンサ状態を送信
-    udp.print(", ");                        // 「,」カンマと「␣」を送信
-    pir = digitalRead(PIN_PIR);              // 人感センサの状態を取得
-    udp.println(pir);                       // 現在のセンサの状態を送信
-    Serial.println(pir);                    // シリアル出力表示
-    udp.endPacket();                        // UDP送信の終了(実際に送信する)
-    delay(10);                              // 送信待ち時間
+    udp.print(DEVICE);                          // デバイス名を送信
+    udp.print(PIR);                             // 起動時のセンサ状態を送信
+    udp.print(", ");                            // 「,」カンマと「␣」を送信
+    pir = digitalRead(PIN_PIR);                 // 人感センサの状態を取得
+    udp.println(pir);                           // 現在のセンサの状態を送信
+    Serial.println(pir);                        // シリアル出力表示
+    udp.endPacket();                            // UDP送信の終了(実際に送信する)
+    delay(10);                                  // 送信待ち時間
 
     HTTPClient http;                            // HTTPリクエスト用インスタンス
     http.setConnectTimeout(15000);              // タイムアウトを15秒に設定する
@@ -102,7 +102,7 @@ void loop(){                                // 繰り返し実行する関数
         http.POST("message=センサが反応しました。Value=" + String(pir)); // メッセージをLINEへ送信する
         http.end();                             // HTTP通信を終了する
     }
-    sleep();                                // sleepを実行
+    sleep();                                    // 下記のsleep関数を実行
 }
 
 void sleep(){
@@ -120,6 +120,7 @@ void sleep(){
     PIR = !pir;
     if(PIR) pir_wake = ESP_GPIO_WAKEUP_GPIO_HIGH;
     else    pir_wake = ESP_GPIO_WAKEUP_GPIO_LOW;
+    led_off();                                  // (WS2812)LEDの消灯
 
     Serial.println("Going to sleep, now...");
     delay(100);                                 // 待ち時間処理
