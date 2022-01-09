70514c1a2da1786edfa338092a25f38380bf3f8d
Wataru KUNINO <git@bokunimo.net>
2022-01-10 00:44:44 +0900

    スワップ操作修正・メモリ節約

--- learning/ex04_lcd/get_html.ino
+++ learning/ex04_lcd/get_html.ino
@@ -3,8 +3,47 @@ HTMLコンテンツ 液晶
 
                                           Copyright (c) 2016-2019 Wataru KUNINO
 *******************************************************************************/
+#define _html_size 567 + 32
 
 String getHtml(String rx){
+    char html[_html_size],s[65],s_ip[16];
+    uint32_t ip = WiFi.localIP();
+    sprintf(s_ip,"%d.%d.%d.%d",
+        ip & 255,
+        ip>>8 & 255,
+        ip>>16 & 255,
+        ip>>24
+    );
+    snprintf(html,_html_size,"<html>\n<head><title>Wi-Fi コンシェルジェ 掲示板担当</title>\n");
+    strcat(html,"<meta http-equiv=\"Content-type\" content=\"text/html; charset=UTF-8\">");
+    strcat(html,"</head>");
+    strcat(html,"<body>");
+    strcat(html,"<h3>LCD STATUS</h3>");
+    strcat(html,"<p>");
+    rx.toCharArray(s, 64);
+    strcat(html,s);
+    strcat(html,"</p>");
+    strcat(html,"<hr>");
+    strcat(html,"<h3>HTTP GET</h3>");
+    sprintf(html,"%s<p>http://%s/?TEXT=文字列</p>",html,s_ip);
+    sprintf(s,"<form method=\"GET\" action=\"http://%s/\">",s_ip);
+    sprintf(html,"%s%s<input type=\"submit\" name=\"TEXT\" value=\"Hello\"></form>",html,s);
+    sprintf(html,"%s%s<input type=\"text\" name=\"TEXT\" value=\"\">",html,s);
+    strcat(html,"<input type=\"submit\" value=\"送信\">");
+    strcat(html,"</form>");
+    strcat(html,"<hr>");
+    strcat(html,"<h3>HTTP POST</h3>");
+    sprintf(s,"<form method=\"POST\" action=\"http://%s/\">",s_ip);
+    sprintf(html,"%s%s<input type=\"submit\" name=\"TEXT\" value=\"Hello\"></form>",html,s);
+    sprintf(html,"%s%s<input type=\"text\" name=\"TEXT\" value=\"\">",html,s);
+    strcat(html,"<input type=\"submit\" value=\"送信\">");
+    strcat(html,"</form>");
+    strcat(html,"</body>");
+    strcat(html,"</html>");
+    Serial.println("sizeof(html)=" + String(strlen(html)+1));
+    return String(html);
+
+    /*
     String S;
     char s[65],s_ip[16];
     uint32_t ip = WiFi.localIP();
@@ -50,4 +89,5 @@ String getHtml(String rx){
     S += "</body>";
     S += "</html>";
     return S;
+    */
 }
