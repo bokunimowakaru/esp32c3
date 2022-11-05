26109c5ac350f43440a6348389437e2dfe1b17b9
Wataru KUNINO <git@bokunimo.net>
2022-11-05 12:35:01 +0900

    HTTPデータ順序の訂正

--- learning/ex10_cam/ex10_cam.ino
+++ learning/ex10_cam/ex10_cam.ino
@@ -110,18 +110,16 @@ void loop(){
         html(client,size,update,WiFi.localIP()); // コンテンツ表示
         client.flush();                     // ESP32用 ERR_CONNECTION_RESET対策
         client.stop();                      // クライアントの切断
-        Serial.print(size);                 // ファイルサイズをシリアル出力表示
-        Serial.println(" Bytes");           // シリアル出力表示
         return;                             // 処理の終了・loop()の先頭へ
     }
     if(strncmp(s,"GET /cam.jpg",12)==0){    // 画像取得指示の場合
-        CamCapture();                       // カメラで写真を撮影する
-        size=CamGetData(client);
+        size=CamCapture();                  // カメラで写真を撮影する
         client.println("HTTP/1.0 200 OK");                  // HTTP OKを応答
         client.println("Content-Type: image/jpeg");         // JPEGコンテンツ
         client.println("Content-Length: " + String(size));  // ファイルサイズ
         client.println("Connection: close");                // 応答後に閉じる
         client.println();                                   // ヘッダの終了
+        CamGetData(client);                 // JPEGデータ送信
         client.println();                   // コンテンツの終了
         client.flush();                     // ESP32用 ERR_CONNECTION_RESET対策
         client.stop();                      // クライアントの切断
